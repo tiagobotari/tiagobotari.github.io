62b303d486da41d4137fda6f658f6e6eded2978d
tiagobotari <tiagobotari@gmail.com>
2026-01-26 19:06:40 +0100

    Fix: remove sitemap plugin causing build error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tiagobotari.github.io',
   integrations: [
-    tailwind(),
-    sitemap()
+    tailwind()
   ],
   output: 'static',
   build: {
